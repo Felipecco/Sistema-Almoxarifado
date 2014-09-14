70b1bd2c172298472555ebd8dfb22497f62310ac
Gean C Rotta <gean.rotta@gmail.com>
2014-09-13 22:20:49 -0300

    Conexao com banco estabelecida
    alteraçoes arquivo de conexao

--- testar.php
+++ testar.php
@@ -1,28 +1,19 @@
-<?
+<?php
+	require('include/conecta.php');
 	session_start();
 	if (!isset($_POST['senha'])){
 		echo "<script>alert(\"Preencha corretamente!\");history.back(-1);</script>";
 		exit;
 	}
-	$senha = addslashes($_POST['senha']);
-
-	@ $base = mysql_connect('localhost','redstyle','saw5fmroot');
-	if (mysql_errno()){
-	echo "ERRO : " . mysql_errno() . "</body></html>";
-	exit;
-	}
-	
-	mysql_select_db("redstyle_evolution", $base);
-	
+	$senha = $_POST['senha'];
 	$sql = "SELECT * FROM usuarios WHERE senha = '$senha'";
 	$tabela = mysql_query($sql, $base);
 	$registro = mysql_num_rows($tabela);
-	
 	if ($registro == 0){
 	header("Location: index.php?errologin=1");
 	exit;
 	} else {
-	$reg = mysql_fetch_array($tabela, MYSQL_ASSOC);
+	$reg = mysql_fetch_array($tabela);
 	$_SESSION['id'] = $reg['id'];
 	$_SESSION['nivel'] = $reg['nivel'];
 	header("Location: intranet.php");
